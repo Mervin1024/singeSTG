5bd98e350a7a345570aedec3845cb2242a64e04c
Mervin1024 <625046839@qq.com>
2015-08-31 11:43:19 +0800

    SteeringWheel

--- singleSTG/SteeringWheel.m
+++ singleSTG/SteeringWheel.m
@@ -25,11 +25,13 @@ NSString *const steetingWheelCenterImage = @"DiscCenter";
         [self addSubview:centerImageView];
         centerImageView.image = [UIImage imageNamed:steetingWheelCenterImage];
         self.image = [UIImage imageNamed:steetingWheelImage];
-//        self.backgroundColor = [UIColor clearColor];
+        
         TouchView *touchView = [[TouchView alloc]initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
         touchView.delegate = self;
         [self addSubview:touchView];
         self.userInteractionEnabled = YES;
+        
+        self.backgroundColor = [UIColor yellowColor];
     }
     return self;
 }
@@ -37,7 +39,27 @@ NSString *const steetingWheelCenterImage = @"DiscCenter";
 - (void)touchView:(TouchView *)view touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     UITouch *touch = [touches anyObject];
     CGPoint point = [touch locationInView:self];
-    NSLog(@"%@",NSStringFromCGPoint(point));
+    CGFloat distance = [self distanceOfPiont:point otherPoint:centerImageView.center];
+    if (distance > self.frame.size.height/2) {
+        return;
+    }else if (distance > self.frame.size.height/2-centerImageView.frame.size.height/2){
+        double scale = (self.frame.size.height/2-centerImageView.frame.size.height/2)/distance;
+        CGPoint newPoint = CGPointMake(point.x*scale-centerImageView.center.x*(scale-1), point.y*scale-centerImageView.center.y*(scale-1));
+        [UIView animateWithDuration:0.2 animations:^{
+            centerImageView.center = newPoint;
+        }completion:^(BOOL finished){
+            
+        }];
+        return;
+    }else{
+        NSTimeInterval time = distance/(self.frame.size.height/2-centerImageView.frame.size.height/2)*0.2;
+        [UIView animateWithDuration:time animations:^{
+            centerImageView.center = point;
+        }completion:^(BOOL finished){
+            
+        }];
+        return;
+    }
 }
 
 - (void)touchView:(TouchView *)view touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
@@ -45,7 +67,11 @@ NSString *const steetingWheelCenterImage = @"DiscCenter";
 }
 
 - (void)touchView:(TouchView *)view touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    
+    centerImageView.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+}
+
+- (CGFloat)distanceOfPiont:(CGPoint)point otherPoint:(CGPoint)otherPoint{
+    return sqrt(pow(point.x-otherPoint.x, 2)+pow(point.y-otherPoint.y, 2));
 }
 
 @end
